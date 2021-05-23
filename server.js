165b93eddfa8a25b4fe2dcb72663973582c335ae
JAYAVEL T <tjayaveljayavel@gmail.com>
2021-05-23 08:10:34 +0530

    f

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const PORT =process.env.PORT;
+const PORT = process.env.PORT;
+const DB_URL=process.env.DB_URL;
 
-
-// mongoose.connect("", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useCreateIndex: true,
-//   }).then(() => {
-//     console.log("Successfully connected to the database");
-//   }).catch(err => {
-//       console.log('Could not connect to the database. Exiting now...', err);
-//       process.exit();
-//   });
+mongoose.connect(DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  }).then(() => {
+    console.log("Successfully connected to the database");
+  }).catch(err => {
+      console.log('Could not connect to the database. Exiting now...', err);
+      process.exit();
+  });
 
 
 app.get('/jayavel' ,function(req,res){
